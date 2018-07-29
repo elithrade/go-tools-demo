c458faead4932c1399065d3af23ea74a3b647b88
Bin Hu <elithrade@gmail.com>
2018-07-29 21:23:15 +0800

    Hello web regexp demo

---
+++ hello-web-regexp/main.go
@@ -0,0 +1,39 @@
+// Package main provides ...
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	// If the return error is not handled
+	// the program will fail silently
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	// regex used to match string starts with anything ends with gmail.com
+	re := regexp.MustCompile("^(.+)@gmail.com$")
+	// Path of the request
+	path := req.URL.Path
+	match := re.FindAllStringSubmatch(path, -1)
+	if match != nil {
+		// We have a match
+		_, err := fmt.Fprintf(w, "Hello, gmail user %s\n", match[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+	_, err := fmt.Fprintf(w, "Hello web")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
