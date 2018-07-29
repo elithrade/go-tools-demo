4437a7c97b500f80c3c21e510bf00a18dd47928a
Bin Hu <elithrade@gmail.com>
2018-07-29 20:32:43 +0800

    More go list and go doc examples

--- go-list/main.go
+++ go-list/main.go
@@ -1,5 +1,9 @@
 // go list -f '{{ .Name }}' will show the package name
 // go list -f '{{ .Name }}: {{ .Doc }}' will show the package name and documentation
+// go list -f '{{ .Imports }}' will show the dependencies
+// go list -f '{{ .Imports }}' <package> will show the dependencies of <package>
+// go doc <package> <function> will show documentations of <function> in <package>
+// godoc -http :<port> will show documentations of installed packages using localhost:<port>
 package main
 
 import (
